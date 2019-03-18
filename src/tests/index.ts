4b5e5ca422bc790c01d79ec6a05737855edbc064
Marc <16156117+Marc-JB@users.noreply.github.com>
2019-03-18 17:19:34 +0100

    Bugfixes

--- src/tests/index.ts
+++ src/tests/index.ts
@@ -2,16 +2,16 @@ import * as chai from "chai"
 import "mocha"
 
 import { ContactPersonController } from "../../build/main/controllers/contactPersonController"
-import { ContactPerson, Http } from "../main/models"
+import { IContactPerson, IHttp } from "../main/models"
 
 chai.should()
 
 describe("ContactPersonController", () => {
     it("getAll should return a list of ContactPersons", async () => {
         // Arrange
-        const contactPersons = [{ id: 1 }, { id: 2 }] as ContactPerson[]
+        const contactPersons = [{ id: 1 }, { id: 2 }] as IContactPerson[]
         const httpResponse = { contactpersons: contactPersons }
-        const fakeHttp = class implements Http {
+        const fakeHttp = class implements IHttp {
             public async get<T>(url: string): Promise<{data: T; status: number}> {
                 return new Promise((resolve, reject) => {
                     resolve({ data: httpResponse as unknown as T, status: 200 })
