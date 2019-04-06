ea724ff44e623d330650311aecd57692075389d6
Marc <16156117+Marc-JB@users.noreply.github.com>
2019-04-06 19:57:10 +0200

    Add User controller

---
+++ src/main/controllers/userController.ts
@@ -0,0 +1,45 @@
+import { Auth, Body, CreateItem, DeleteItem, ID, Resource } from "@peregrine/webserver"
+
+import { IRepository } from "../database/repository"
+import { User } from "../models/user"
+
+@Resource("user")
+export class UserController {
+    public constructor(protected readonly userRepository: IRepository<User>) {}
+
+    @CreateItem()
+    public async createUser(@Body() body: unknown): Promise<User> {
+        let user
+        try {
+            user = new User(body)
+        } catch (error) {
+            throw {
+                code: 400,
+                body: {
+                    errorName: "Bad Request",
+                    errorMessage: (error as Error).message,
+                },
+            }
+        }
+
+        return this.userRepository.create(user)
+    }
+
+    @DeleteItem()
+    public async deleteUser(@ID() id: string, @Auth() user: null | User): Promise<object> {
+        if (user === null || user._id !== id) {
+            throw {
+                code: 401,
+                body: {
+                    errorName: "Unauthorised",
+                },
+            }
+        }
+
+        const userObject = await this.userRepository.getById(id)
+        userObject.isActive = false
+        await this.userRepository.update(id, userObject)
+
+        return {}
+    }
+}
