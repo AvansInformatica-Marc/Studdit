a9d4b6d7fa6ee8b1ef50af491630f6178e17e3de
Marc <16156117+Marc-JB@users.noreply.github.com>
2019-04-07 15:14:05 +0200

    Add updatePassword

--- src/main/controllers/userController.ts
+++ src/main/controllers/userController.ts
@@ -1,5 +1,5 @@
 import { http } from "@peregrine/exceptions"
-import { Auth, Body, CreateItem, DeleteItem, ID, Resource } from "@peregrine/webserver"
+import { Auth, Body, CreateItem, DeleteItem, ID, Resource, UpdateItem, JsonObject } from "@peregrine/webserver"
 
 import { IRepository } from "../database/repository"
 import { User } from "../models/user"
@@ -18,6 +18,25 @@ export class UserController {
         }
 
         return this.userRepository.create(user)
+    }
+
+    @UpdateItem()
+    public async updatePassword(@ID() id: string, @Body() body: unknown, @Auth() user: null | User): Promise<User> {
+        if (user === null || user._id !== id) {
+            throw new http.Unauthorised401Error()
+        }
+
+        const userObject = await this.userRepository.getById(id)
+        if (userObject === null) {
+            throw new http.NotFound404Error("Error 422 invalid ID")
+        }
+        if (typeof body !== "object" || body === null || typeof (body as JsonObject).password !== "string") {
+            throw new http.BadRequest400Error()
+        }
+        userObject.password = (body as JsonObject).password as string
+        await this.userRepository.update(id, userObject)
+
+        return userObject
     }
 
     @DeleteItem()
